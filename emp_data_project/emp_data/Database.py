9df56b1850d08a1393f2888e8342562120454407
Peyton Sneddon <10823753@my.uvu.edu>
2022-10-27 16:31:01 -0600

    add methods to database class

--- emp_data_project/emp_data/Database.py
+++ emp_data_project/emp_data/Database.py
@@ -1,4 +1,45 @@
 #TODO outline database class, attr, methods
 
 class Database:
-    pass
+    def __init__(self):
+        pass
+
+    def load_emp(self):
+        # Brings up Employee and info concerning said employee
+        pass
+
+    def save_emp(self):
+        # Saves data of employee into employee database
+        pass
+
+    def create_emp(self):
+        # Allows creation of new employee data to add
+        pass
+
+    def deactivate_emp(self):
+        # Removes employee from database in files
+        pass
+
+    def update_emp(self):
+        # Allows for changing of employee information by those with admin permissions
+        pass
+
+    def update_self(self):
+        # Allows changing of personal information for those without admin permissions
+        pass
+
+    def merge_database(self):
+        # This method combines the data of separate files to form new database file for system
+        pass
+
+    def report_database(self):
+        # Produces report of information within the database for the user to view
+        pass
+
+    def validate_form(self):
+        # This formula verifies whether the specified file concerning the data entered is correct and valid according to data constraints
+        pass
+
+    def import_database(self):
+        # Permits transfer of data from separate file for system
+        pass
