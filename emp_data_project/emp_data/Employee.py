026ca4210dfcf9020d952941b166183a54b82101
Bryce Gardner <bgardner7@gmail.com>
2022-10-31 13:35:09 -0600

    Commented out methods and additional classes

--- emp_data_project/emp_data/Employee.py
+++ emp_data_project/emp_data/Employee.py
@@ -5,14 +5,14 @@ from types import CoroutineType
 
 
 class Employee:
-    def __init__(self, emp_id, first_name, last_name, city, state, zip, pay_classification, ssn, start_date, bank_info, dob, access_codes, job_title, job_dept, email):
+    def __init__(self, emp_id, first_name, last_name, city, state, zipcode, classification, ssn, start_date, bank_info, dob, access_codes, job_title, job_dept, email):
         self.emp_id = emp_id
         self.first_name = first_name
         self.last_name = last_name
         self.city = city
         self.state = state
-        self.zip = zip
-        self.pay_classification = pay_classification
+        self.zipcode = zipcode
+        self.classification = classification
         self.ssn = ssn
         self.start_date = start_date
         self.bank_info = bank_info
@@ -22,8 +22,15 @@ class Employee:
         self.job_dept = job_dept
         self.email = email
 
-    def make_hourly(self):
-        self.pay_classification = 'hourly'
+        '''if classification == '3':
+            self.classification = Hourly(rate)
+        elif classification == '1':
+            self.classification = Salaried(salary)
+        elif classification == '2':
+            self.classification = Commissioned(salary,comm_rate)'''
+
+    '''def make_hourly(self,rate):
+        self.pay_classification = Hourly(rate)
 
     def make_salaried(self):
         self.pay_classification = 'salaried'
@@ -35,4 +42,20 @@ class Employee:
         pass
 
     def generate_pay_report(self):
-        pass
+        pass'''
+
+'''class Hourly(Classification):
+    def __init__(self,hourly_rate):
+        self.hourly_rate = float(hourly_rate)
+        self.hours_worked = []
+    
+    def add_timecard(self, hours):
+        self.hours_worked.append(hours)
+
+    def issue_payment(self):
+        total_hours = 0
+        for hour in self.hours_worked:
+            total_hours += hour
+        pay = total_hours * self.hourly_rate
+        self.hours_worked = []
+        return float(pay)'''
