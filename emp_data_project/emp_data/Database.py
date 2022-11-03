0293b7cad9ca13f84488c3642657c1810d7d69e5
Peyton Sneddon <10823753@my.uvu.edu>
2022-11-03 14:24:26 -0600

    database update

--- emp_data_project/emp_data/Database.py
+++ emp_data_project/emp_data/Database.py
@@ -3,8 +3,31 @@
 class Database:
     def __init__(self):
         self.data = [[]]
+        self.cur_emp = ''
 
-    def load_emp(self, cur_emp):
+    def build_database():
+        # creates a database that is based on employee IDs like so:
+        '''
+        [[1],[2],[3],[4],[5],[6],[7],[8],[9]
+        [10],[11],[12],[13],[14],[15],[16],[17],[18],[19]
+        ...
+        ...
+        ...
+        [91],[92],[93],[94],[95],[96],[97],[98],[99]]
+        '''
+        # to find and insert data:
+        '''
+        if ID_number < 10:
+            row = 0
+            col = ID_number - 1
+
+        else:
+            col = (ID_number % 10) - 1
+            row = (ID_number // 10) - 1
+        '''
+
+        pass
+    def load_emp(self, cur_emp=''):
         # Brings up Employee and info concerning said employee
         pass
 
@@ -18,6 +41,7 @@ class Database:
 
     def deactivate_emp(self, cur_emp):
         # Removes employee from database in files
+        # Flag ID so that new employees can use it
         pass
 
     def update_emp(self):
