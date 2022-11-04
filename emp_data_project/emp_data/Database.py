6897e2999c3317c47b1ce007693b28ed8cbb6e34
Peyton Sneddon <10823753@my.uvu.edu>
2022-11-04 09:30:39 -0600

    database

--- emp_data_project/emp_data/Database.py
+++ emp_data_project/emp_data/Database.py
@@ -1,10 +1,30 @@
 #TODO outline database class, attr, methods
 # Questions for meeting:
 # how to save between runs
+import Employee
+class Fake_employee:
+    def __init__(self):
+        self.employees = [[]]
+        self.cur_emp = ''
+        
 class Database:
     def __init__(self):
-        self.data = [[]]
+        self.employees = [[]]
         self.cur_emp = ''
+
+    def load_employees(self):
+        IN_FILE_NAME = "emp_data_project/emp_data/test.csv"
+        
+        emp_file = open(IN_FILE_NAME, 'r')
+        test_emp = Employee()
+        emp_file.readline()
+        for line in emp_file:
+            line_list = line.strip().split(",")
+
+            self.employees.append(Employee(line_list[0],line_list[1],line_list[2],line_list[3],line_list[4],line_list[5],line_list[6],line_list[7],line_list[8]\
+                ,line_list[9],line_list[10],line_list[11],line_list[12],line_list[13],line_list[14],line_list[15],line_list[16],line_list[17]))
+
+        print(self.employees)
 
     def build_database():
         # creates a database that is based on employee IDs like so:
@@ -72,3 +92,5 @@ class Database:
     def store_database(self):
         #saves the database as txt before closing
         pass
+
+my_database = Database()
